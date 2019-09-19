b91c7d6e39ad5c98c80bed4768652e294c7aaa2c
Annie <anniekao@trentu.ca>
2019-09-19 16:33:28 +0000

    added sales index route and sales controller

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:destroy]
+    resources :sales, only: [:index]
   end
 
   get 'about', to: 'about#show'
